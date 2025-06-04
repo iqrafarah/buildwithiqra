9cace123ea651bb7155c026582e9cf069795c951
iqra farah <iqranur542@hotmail.com>
2025-06-04 21:25:57 +0200

    hero slider

--- assets/js/mySwiper.js
+++ assets/js/mySwiper.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const heroSwiper = new Swiper(".hero-swiper", {
-    direction: "horizontal",
     loop: true,
-    grabCursor: false,
-    // autoplay: {
-    //   disableOnInteraction: false,
-    //   pauseOnMouseEnter: false,
-    // },
-    speed: 2000,
-    spaceBetween: 20,
-    breakpoints: {
-      320: { slidesPerView: 1.5 },
-      480: { slidesPerView: 2.5 },
-      768: { slidesPerView: 4.5 },
+    grabCursor: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
     },
+    speed: 3000,
+    spaceBetween: 20,
   });
 });
